2be589949e300b9cf2577e1fef6dd987418cf0ed
Nataly Salazar <natalysalazarglez@outlook.com>
2022-10-10 17:01:43 -0500

    feat: login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tuks_divide/pages/login_page/login_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,14 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/pages/login_page/login_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginPage extends StatelessWidget {
+  var _emailController = TextEditingController();
+  var _passwordController = TextEditingController();
+  LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tuks Divide'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15.0, 35.0, 15.0, 8.0),
+            child: TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(label: Text("Email")),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15.0, 8.0, 15.0, 8.0),
+            child: TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(label: Text("Contraseña")),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15.0, 18.0, 15.0, 30.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text("INICIAR SESIÓN"),
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(1.0),
+                  )),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15.0, 8.0, 15.0, 30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Flexible(
+                  child: Divider(
+                    thickness: 1,
+                    indent: MediaQuery.of(context).size.width / 20,
+                    endIndent: MediaQuery.of(context).size.width / 20,
+                  ),
+                ),
+                const Text("O"),
+                Flexible(
+                  child: Divider(
+                    thickness: 1,
+                    indent: MediaQuery.of(context).size.width / 20,
+                    endIndent: MediaQuery.of(context).size.width / 20,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15.0, 8.0, 15.0, 8.0),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: const FaIcon(FontAwesomeIcons.google),
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  backgroundColor: const Color.fromARGB(0xFF, 0xDC, 0x4E, 0x41),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(1.0),
+                  )),
+              label: const Text("INICIAR SESIÓN CON GOOGLE"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15.0, 50.0, 15.0, 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("¿No tienes una cuenta? "),
+                GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    "Crear una cuenta",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
