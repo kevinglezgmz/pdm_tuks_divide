408c158a62c4ca27382611d7ca2109da55b06fe1
Nataly Salazar <natalysalazarglez@outlook.com>
2022-10-12 22:05:44 -0500

    fix: rollback in main

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: CreateGroupPage(),
+      home: LoginPage(),
     );
   }
 }
