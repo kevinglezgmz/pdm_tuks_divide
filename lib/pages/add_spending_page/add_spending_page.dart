0f09f540968345e9604d777e6ca74e79a0626584
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2022-10-14 19:07:04 -0500

    feat: added the spending distribution page with three different types of distributions

--- lib/pages/add_spending_page/add_spending_page.dart
+++ lib/pages/add_spending_page/add_spending_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:tuks_divide/utils/text_input_utils.dart';
 
 class AddSpendingPage extends StatelessWidget {
   final TextEditingController descriptionController = TextEditingController();
@@ -204,31 +204,5 @@ class AddSpendingPage extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class DecimalTextInputFormatter implements TextInputFormatter {
-  late RegExp _exp;
-
-  DecimalTextInputFormatter({
-    int? decimalRange,
-  }) : assert(decimalRange == null || decimalRange >= 0,
-            'DecimalTextInputFormatter declaretion error') {
-    String dp = (decimalRange != null && decimalRange > 0)
-        ? "([.][0-9]{0,$decimalRange}){0,1}"
-        : "";
-    String num = "[0-9]*$dp";
-    _exp = RegExp("^($num){0,1}\$");
-  }
-
-  @override
-  TextEditingValue formatEditUpdate(
-    TextEditingValue oldValue,
-    TextEditingValue newValue,
-  ) {
-    if (_exp.hasMatch(newValue.text)) {
-      return newValue;
-    }
-    return oldValue;
   }
 }
